a77764cb81ed601dadda186b383554000f0fb1ea
Kenzo8845 <silvabazanikenai@gmail.com>
2025-06-03 21:19:44 -0300

    Exercicio 5 Codifica

---
+++ Ex5.dart
@@ -0,0 +1,11 @@
+void main(){
+  String valor1 = "30";
+  String valor2 = "15.75";
+
+  double v1 = double.parse(valor1);
+  double v2 = double.parse(valor2);
+
+  double soma = v1 + v2;
+  print("a soma de $v1 e $v2 e igual a $soma");
+
+}
